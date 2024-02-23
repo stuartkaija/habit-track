e7b5c69da1295cee41855e049643af180f69fccc
Stuart Kaija <stuartkaija@gmail.com>
2024-02-22 20:14:38 -0800

    alert container sticky

--- src/components/AlertContainer.tsx
+++ src/components/AlertContainer.tsx
@@ -2,7 +2,7 @@ import AlertNotification from "./AlertNotification"
 
 export default function AlertContainer({ alerts }: any) {
   return (
-    <div className="absolute z-50 w-96 m-auto left-0 right-0 top-2">
+    <div className="fixed z-50 w-96 m-auto left-0 right-0 top-2">
       {alerts.map((alert: any) => {
         console.log(alert)
         return (
