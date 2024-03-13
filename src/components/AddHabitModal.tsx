ce49f7db35bf916d58788a997dbeb43681f6ecee
Stuart Kaija <stuartkaija@gmail.com>
2024-03-13 11:26:38 -0700

    add habit selected days state to add habit modal

--- src/components/AddHabitModal.tsx
+++ src/components/AddHabitModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, FormGroup, FormControlLabel, Checkbox, Box } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 import { isBefore, startOfYear, endOfYear } from 'date-fns';
 import { useAuth } from '../lib/AuthProvider';
@@ -8,6 +8,8 @@ import { supabase } from '../supabaseClient';
 import HabitGenerator from '../classes/HabitGenerator';
 
 const generator = new HabitGenerator;
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 export default function AddHabitModal({
   modalOpen,
@@ -20,10 +22,12 @@ export default function AddHabitModal({
   const lastDayOfYear = endOfYear(new Date());
 
   const [habitName, setHabitName] = useState<string>('');
+  const [selectedDays, setSelectedDays] = useState([false, false, false, false, false, false, false])
   const [startDate, setStartDate] = useState<Date>(firstDayOfYear);
   const [endDate, setEndDate] = useState<Date>(lastDayOfYear);
   const [nameError, setNameError] = useState<boolean>(false);
   const [dateError, setDateError] = useState<boolean>(false);
+  const [selectedDaysError, setSelectedDaysError] = useState<boolean>(false);
 
   const auth = useAuth();
   const alert = useAlert();
@@ -34,6 +38,22 @@ export default function AddHabitModal({
     setEndDate(lastDayOfYear);
     setNameError(false);
     setDateError(false);
+    setSelectedDaysError(false);
+  }
+
+  const handleSelectDays = (position: number) => {
+    const updatedDays = selectedDays.map((day, index) => index === position ? !day : day)
+    setSelectedDays(updatedDays);
+  }
+
+  const handleSelectAllDays = () => {
+    if (selectedDays.every(day => day === true)) {
+      const updatedDays = selectedDays.map(() => false);
+      setSelectedDays(updatedDays);
+    } else {
+      const updatedDays = selectedDays.map(() => true);
+      setSelectedDays(updatedDays);
+    }
   }
 
   const handleAddNewHabit = async (event: React.SyntheticEvent) => {
@@ -49,6 +69,11 @@ export default function AddHabitModal({
       return;
     }
 
+    if (selectedDays.every(day => !day)) {
+      setSelectedDaysError(true);
+      return;
+    }
+
     // generate habit data
     const newHabitData = generator.generateHabitData();
 
@@ -59,7 +84,8 @@ export default function AddHabitModal({
         name: habitName,
         start_date: startDate,
         end_date: endDate,
-        completion_data: newHabitData
+        completion_data: newHabitData,
+        days: selectedDays
       })
 
     if (error) {
@@ -79,51 +105,97 @@ export default function AddHabitModal({
         resetAllState();
         handleOpenModal();
       }}
+    // PaperProps={{
+    //   sx: {
+    //     height: '100vh'
+    //   }
+    // }}
     >
       <DialogTitle>Add Habit</DialogTitle>
       <DialogContent>
         <DialogContentText>Fill out the details for the habit you would like to track.</DialogContentText>
+        <form className='flex flex-col m-2 space-y-4' onSubmit={handleAddNewHabit}>
+          <TextField
+            type='text'
+            id='name'
+            label='Habit Name'
+            value={habitName}
+            onChange={(event) => {
+              setNameError(false);
+              setHabitName(event.target.value)
+            }
+            }
+            error={nameError}
+            helperText='name of habit - 100 characters or less'
+          />
+          <fieldset className={`border p-3 rounded-md ${selectedDaysError ? 'border-red-600' : 'border-slate-200'}`}>
+            <legend className={`text-xs ${selectedDaysError ? 'text-red-600' : 'text-gray-500 '}`}>Select Days To Complete Habit</legend>
+            <FormGroup>
+              <FormControlLabel
+                label='Select All'
+                control={
+                  <Checkbox
+                    checked={selectedDays.every(day => day === true)}
+                    indeterminate={selectedDays.some(day => day === true)}
+                    onChange={() => handleSelectAllDays()}
+                  />
+                }
+              />
+              {days.map((day, index) => {
+                return (
+                  <FormControlLabel
+                    sx={{
+                      ml: 0.1
+                    }}
+                    label={day}
+                    value={index}
+                    control={
+                      <Checkbox
+                        checked={selectedDays[index]}
+                        onChange={() => handleSelectDays(index)}
+                      />
+                    }
+                  />
+                )
+              })}
+              {/* <FormControlLabel control={<Checkbox />} label='Sunday' value={0} />
+              <FormControlLabel control={<Checkbox />} label='Monday' value={1} />
+              <FormControlLabel control={<Checkbox />} label='Tuesday' value={2} />
+              <FormControlLabel control={<Checkbox />} label='Wednesday' value={3} />
+              <FormControlLabel control={<Checkbox />} label='Thursday' value={4} />
+              <FormControlLabel control={<Checkbox />} label='Friday' value={5} />
+              <FormControlLabel control={<Checkbox />} label='Saturday' value={6} /> */}
+            </FormGroup>
+          </fieldset>
+          <p className={`${selectedDaysError ? 'visible' : 'invisible'} text-xs mt-0 text-red-500`}>You must select at least one day on which to complete habit.</p>
+          <DatePicker
+            label='Start Date'
+            value={startDate}
+            minDate={firstDayOfYear}
+            maxDate={lastDayOfYear}
+            onChange={(value) => {
+              setDateError(false);
+              setStartDate(value!);
+            }}
+          />
+          <DatePicker
+            label='End Date'
+            value={endDate}
+            minDate={firstDayOfYear}
+            maxDate={lastDayOfYear}
+            onChange={(value) => {
+              setDateError(false);
+              setEndDate(value!);
+            }}
+          />
+          <p className={`${dateError ? 'visible' : 'invisible'} text-xs text-red-500`}>End Date must be after Start Date</p>
+        </form>
       </DialogContent>
-      <form className='flex flex-col justify-around m-2 h-80' onSubmit={handleAddNewHabit}>
-        <TextField
-          type='text'
-          id='name'
-          label='Habit Name'
-          value={habitName}
-          onChange={(event) => {
-            setNameError(false);
-            setHabitName(event.target.value)
-          }
-          }
-          error={nameError}
-          helperText='name of habit - 100 characters or less'
-        />
-        <DatePicker
-          label='Start Date'
-          value={startDate}
-          minDate={firstDayOfYear}
-          maxDate={lastDayOfYear}
-          onChange={(value) => {
-            setDateError(false);
-            setStartDate(value!);
-          }}
-        />
-        <DatePicker
-          label='End Date'
-          value={endDate}
-          minDate={firstDayOfYear}
-          maxDate={lastDayOfYear}
-          onChange={(value) => {
-            setDateError(false);
-            setEndDate(value!);
-          }}
-        />
-        <p className={`${dateError ? 'visible' : 'invisible'} text-xs text-red-500`}>End Date must be after Start Date</p>
-        <DialogActions>
-          <button className='m-2 p-2 w-2/3 rounded-sm border border-slate-800 hover:bg-green-500 hover:text-white transition-colors' type='submit'>Add</button>
-          <button className='m-2 p-2 w-1/3 rounded-sm border border-slate-800 hover:bg-slate-400 hover:text-white transition-colors' type='reset' onClick={handleOpenModal}>Cancel</button>
-        </DialogActions>
-      </form>
+      <DialogActions>
+        <button className='m-2 p-2 w-2/3 rounded-sm border border-slate-800 hover:bg-green-500 hover:text-white transition-colors' type='submit' onClick={handleAddNewHabit}>Add</button>
+        <button className='m-2 p-2 w-1/3 rounded-sm border border-slate-800 hover:bg-slate-400 hover:text-white transition-colors' type='reset' onClick={handleOpenModal}>Cancel</button>
+      </DialogActions>
+
     </Dialog>
   )
 }
